e5596e204cdc69311848c2199dc86797b653f0a3
Ilkhom I. Melibaev <107622192+fluttberry@users.noreply.github.com>
2025-03-07 08:31:59 +0600

    updated

--- lib/ui/cats_home_gape.dart
+++ lib/ui/cats_home_gape.dart
@@ -1,5 +1,6 @@
 import 'package:cats_app/model/cat.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class CatsHomePage extends StatefulWidget {
   const CatsHomePage({super.key});
@@ -22,9 +23,25 @@ class _CatsHomePageState extends State<CatsHomePage> {
                       ? Image.network(catModel!.url)
                       : CircularProgressIndicator(),
             ),
+            InkWell(onTap: getCat
+
+            , child: Text('Next'),),
           ],
         ),
       ),
     );
+  }
+
+  getCat () async{
+setState(() {
+  catModel = null;
+});
+
+var response = await http.get(Uri.parse('https://cataas.com/cat'));
+if (response.statusCode==200) {
+setState(() {
+  catModel = CatModel.fromJson(response.body);
+});
+}
   }
 }
